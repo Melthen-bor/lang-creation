4dce9a094dc898dee1b1c1f10d6f11f7f2832c1e
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-24 13:03:00 -0800

    Create SettingBoundsException.java

---
+++ SettingBoundsException.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class SettingBoundsException extends Exception{
+  public SettingBoundsException(String errorMessage){
+    super(erroMessage);
+  }
+}
