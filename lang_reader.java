87d2cbd7c33afeac5eac0599b1dc3cc03fb76c5c
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-24 13:31:09 -0800

    Update lang_reader.java

--- lang_reader.java
+++ lang_reader.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.random;
 public class lang_reader{
   public static String Concate(Object o0,Object o1){
     return o0+""+o1;
@@ -22,16 +23,42 @@ public class lang_reader{
       String[255] roots;
       String[255] rgender;
       String[255][255] affixes;
+      String[] consonants = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"};
+      String[] vowels = {"a","e","i","o","u"};
       int ex = 1;
       int set;
+      Random gen = new Random();
+      int randindex;
       if(encode_pos==0){
         if(gender_no_pos==0){
-          set=0;
+          set = 0;
         } else{
-          set=1;
+          set = 1;
         }
       } else{
-        set=2;
+        set = 2;
+      }
+      int z = 0;
+      int y = 0;
+      int w;
+      String x = '';
+      while(ex==1){
+        try{
+          w=gen.nextInt();
+          while(w>0){
+            randindex = gen.nextInt(consonants.length);
+            x=concate(x,consonants[randindex]);
+            randindex = gen.nextInt(vowels.length);
+            x=concate(x,vowels[randindex]);
+            w-=1;
+          }
+          roots[z] = x;
+          z += 1;
+          x = "",
+        } catch(Exception e){
+          z=0;
+          ex=0;
+        }
       }
       if(set==0){
         while(ex==1){
